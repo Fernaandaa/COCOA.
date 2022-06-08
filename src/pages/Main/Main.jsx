8916a71b8e1793b1e728c1b393e268c64b025570
Fernaandaa <Lafernandacz@gmail.com>
2022-06-08 14:03:18 -0500

    update expanding cards

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,22 @@
 import './Main.css'
 import Header from '../../components/Header/Header';
+import Bannerfx from '../../components/Banner/Banner';
+import imgBanner from '../../img/cakeshop/cakes/101001.jpg'
+import Subtitle from '../../components/Subtitle/Subtitle'
+import Expanding from '../../components/Expanding/Expanding'
+import Footer from '../../components/Footer/Footer'
+
 function Main(){
     return(
+        <div>
         <Header/>
+        <Bannerfx background = {imgBanner} />
+        <Subtitle  text = "NOSOTROS"/>
+        <Expanding />
+        <Subtitle  text = "CONTÁCTANOS"/>
+
+        <Footer />
+        </div>
 
     );
 }
