5c2f48a0cad056a62de4002db7bc3e09ab45d0ff
EstefaMH <98820291+EstefaMH@users.noreply.github.com>
2022-06-08 14:06:37 -0500

    modified routes, components, add about folder and create component

--- src/App/routes.js
+++ src/App/routes.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter ,Route , Routes } from "react-router-dom"
-import Home from '../pages/Home/Home'
-import Main from '../pages/Main/Main'
+import Home     from '../pages/Home/Home'
+import Main     from '../pages/Main/Main'
 import Cakeshop from '../pages/Cakeshop/Cakeshop'
 import Biscuits from '../pages/Cakeshop/Biscuits/Biscuits'
 import Cakes    from '../pages/Cakeshop/Cakes/Cakes'
 import Desserts from '../pages/Cakeshop/Desserts/Desserts'
 import GalleryC from '../pages/Cakeshop/GalleryCakeshop/GalleryCakeshop'
-import Bakery from '../pages/Bakery/Bakery'
+import Bakery   from '../pages/Bakery/Bakery'
 import Comprehensive from '../pages/Bakery/Comprehensive/Comprehensive'
-import Handcraft from '../pages/Bakery/Handcrafted/Handcafted'
-import Puffpastries from '../pages/Bakery/Puffpastries/Puffpastries'
-import GalleryB  from '../pages/Bakery/GalleryBakery/GalleryBakery'
-import Shopping from '../pages/ShoppingCart/ShoppingCart'
+import Handcraft     from '../pages/Bakery/Handcrafted/Handcafted'
+import Puffpastries  from '../pages/Bakery/Puffpastries/Puffpastries'
+import GalleryB      from '../pages/Bakery/GalleryBakery/GalleryBakery'
+import Shopping      from '../pages/ShoppingCart/ShoppingCart'
 
 
 function AppRoutes() {
@@ -22,7 +22,7 @@ function AppRoutes() {
                 <Route path='/' element ={<Home />}></Route>
                 <Route path='main/' element ={<Main/>}></Route>
                 
-                <Route path='cakeshop/' element ={<Cakeshop />}>
+                <Route path='main/cakeshop/' element ={<Cakeshop />}>
                     <Route  path='biscuits' element={<Biscuits />}></Route>
                     <Route  path='cakes' element={<Cakes />}></Route>
                     <Route  path='desserts' element={<Desserts />}></Route>
@@ -31,14 +31,14 @@ function AppRoutes() {
 
                 
 
-                <Route path='bakery/' element ={<Bakery />}>
+                <Route path='main/bakery/' element ={<Bakery />}>
                     <Route path='comprenhensive' element={<Comprehensive />}></Route>
                     <Route path='handcraft' element={<Handcraft/>}></Route>
                     <Route path='puffpastries' element={<Puffpastries />}></Route>
                     <Route path='gallery' element={<GalleryB />}></Route>
                 </Route>
 
-                <Route path="/shop" element={<Shopping />}></Route>
+                <Route path="main/shop" element={<Shopping />}></Route>
 
             </Routes>
         </BrowserRouter>  
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,23 @@
 import './Main.css'
 import Header from '../../components/Header/Header';
+import img from '../../img/cakeshop/cakes/101001.jpg'
+import Bannerfx from '../../components/Banner/Banner'
+import Subtitle  from '../../components/Subtitle/Subtitle';
+import Footer from '../../components/Footer/Footer'
+import About from '../../components/About/About';
+
 function Main(){
     return(
+        <div>
         <Header/>
+        <Bannerfx background={img}/>
+        <Subtitle text="NOSOTROS" />
+        <About />
+        <Footer />
+        </div>
 
-    );
+    )
+
 }
+
 export default Main;
