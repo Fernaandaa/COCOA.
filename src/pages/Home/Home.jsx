ec9212e90cd554770bdaa8f22d99dfa24fe06934
Fernaandaa <Lafernandacz@gmail.com>
2022-06-16 18:35:05 -0500

    Start complete and finish button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import './Home.css'
 import { Link } from 'react-router-dom';
 import logo from '../../img/Logo- COCOA.png'
+import { FaArrowRight } from 'react-icons/fa';
+
 function Home() {
     return (
         <div className='main-container-home'>
             <div className='container-right-home'>
                 <div className='container-btn-home'>
-                    <Link to='/main'><button className='btn-home hvr-push hvr-ripple-out'> TIENDA ONLINE</button></Link>
+                <Link to='/main' className='l-button'><button className='{/btn-home hvr-push hvr-ripple-out/} cta'>TIENDA ONLINE <FaArrowRight className='i-button' /></button></Link>
                 </div>
                 <div className='container-logo-home'>
                     <img src={logo} />
